055f0d37ad95dc8b90c10273965aaa9b339743dd
Evgenia <118038480+Rey1147@users.noreply.github.com>
2024-05-23 18:17:17 +0300

    connection db

--- server/src/category/entities/category.entity.ts
+++ server/src/category/entities/category.entity.ts
@@ -1,1 +1,34 @@
-export class Category {}
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinColumn,
+    ManyToOne, OneToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+import { User } from '../../user/entities/user.entity';
+import { Transaction } from '../../transaction/entities/transaction.entity';
+
+@Entity()
+export class Category {
+    @PrimaryGeneratedColumn({name: 'category_id'})
+    id: number
+
+    @Column()
+    title: string
+
+    @ManyToOne(() => User, (user) => user.categories)
+    @JoinColumn({name: 'user_id'})
+    user: User
+
+    @OneToMany(() => Transaction, (transaction) => transaction.category)
+    @JoinColumn({name: 'transaction_id'})
+    transactions: Transaction[]
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updateAt: Date
+}
--- server/src/user/entities/user.entity.ts
+++ server/src/user/entities/user.entity.ts
@@ -1,4 +1,32 @@
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Category } from '../../category/entities/category.entity';
+import { Transaction } from '../../transaction/entities/transaction.entity';
+
+@Entity()
 export class User {
-  email: string;
-  password: string;
+  @PrimaryGeneratedColumn({name: 'user_id'})
+  id: number
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+  @OneToMany(() => Category, (category) => category.user, {
+    onDelete: 'CASCADE'
+  })
+  categories: Category[]
+
+  @OneToMany(() => Transaction, (transaction) => transaction.user, {
+    onDelete: 'CASCADE'
+  })
+  transactions: Transaction[]
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updateAt: Date
+
 }
